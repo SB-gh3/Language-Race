3d6ab1863ef8120a3cd4f916a1c4129be7346e1d
installer <installer@gmail.com>
2025-10-01 05:43:21 -0400

    Removed echo from cd -

--- run.sh
+++ run.sh
@@ -9,31 +9,31 @@ read -p "Enter absolute path to output folder: " out
 #Run Rust
 cd Rust
 cargo run -- $str "$file" "$out/output-rust.txt"
-cd -
+cd ~-
 
 #Run C++
 cd C++
 g++ lev.cpp -o lev
 ./lev $str "$file" "$out/output-cpp.txt"
-cd -
+cd ~-
 
 #Run Go
 cd Go
 go run lev.go $str "$file" "$out/output-go.txt"
-cd -
+cd ~-
 
 #Run Python
 cd Python
 python ./lev.py $str "$file" "$out/output-py.txt"
-cd -
+cd ~-
 
 #Run Java
 cd Java
 javac -cp javatuples-1.2.jar lev.java
 java -cp ".:javatuples-1.2.jar" lev $str "$file" "$out/output-java.txt"
-cd -
+cd ~-
 
 #Run Haskell
 cd Haskell
 runhaskell ./lev.hs $str "$file" "$out/output-haskell.txt"
-cd -
+cd ~-
